fec2584e1a88c5beb742e649e55376fd52a51511
sya-feera <113223505+sya-feera@users.noreply.github.com>
2023-10-12 20:21:47 +0700

    Add files via upload

---
+++ src/projectpemvis_modul2/Projectpemvis_modul2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectpemvis_modul2;
+
+/**
+ *
+ * @author firak
+ */
+public class Projectpemvis_modul2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
